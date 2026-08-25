83bbf17ca5da9d5557b6f44bf2c14c4dc08042fd
Prashanth Puranik <puranik@digdeeper.in>
2026-08-25 11:50:29 +0530

    Step 15 - Built-in pipe

--- demos/workshops-app/src/app/workshops/workshops-list/item/item.ts
+++ demos/workshops-app/src/app/workshops/workshops-list/item/item.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import IWorkshop from '../../models/IWorkshop';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-item',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, DatePipe ],
   templateUrl: './item.html',
   styleUrl: './item.scss',
 })
