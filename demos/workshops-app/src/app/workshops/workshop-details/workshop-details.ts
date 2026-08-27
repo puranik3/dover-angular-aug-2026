d4edbf82fe2ff458ed23135f0f627fc12aaa0573
Prashanth Puranik <puranik@digdeeper.in>
2026-08-27 11:06:12 +0530

    Step 21

---
+++ demos/workshops-app/src/app/workshops/workshop-details/workshop-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workshop-details',
+  imports: [],
+  templateUrl: './workshop-details.html',
+  styleUrl: './workshop-details.scss',
+})
+export class WorkshopDetails {}
--- demos/workshops-app/src/app/workshops/workshops.routes.ts
+++ demos/workshops-app/src/app/workshops/workshops.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { WorkshopsList } from './workshops-list/workshops-list';
 import { AddWorkshop } from './add-workshop/add-workshop';
 import { Favorites } from './favorites/favorites';
+import { WorkshopDetails } from './workshop-details/workshop-details';
 
 export const routes: Routes = [
     {
@@ -20,4 +21,9 @@ export const routes: Routes = [
         component: Favorites,
         title: 'Favorite workshops'
     },
+    {
+        path: 'workshops/:id',
+        component: WorkshopDetails,
+        title: 'Workshop Details'
+    }
 ];
