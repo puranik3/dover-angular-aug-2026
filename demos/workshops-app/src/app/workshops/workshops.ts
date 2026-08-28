e7b75ef09004ec3f64cc0c0f080b68b4f15d06cc
Prashanth Puranik <puranik@digdeeper.in>
2026-08-28 10:57:10 +0530

    Step 27

--- demos/workshops-app/src/app/workshops/workshops.ts
+++ demos/workshops-app/src/app/workshops/workshops.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import IWorkshop from './models/IWorkshop';
 
+import { environment } from '../../environments/environment';
+
 // Root injector will have the new Workshops() object - any part of the app can get this workshops object
 @Injectable({
   providedIn: 'root',
 })
 export class Workshops {
+  private apiUrl = environment.apiUrl;
+
   constructor( private http: HttpClient ) {
   }
 
   getWorkshops( page: number = 1  ) {
     // this.http.get(), post() etc. return an RxJS Observable object
     return this.http.get<IWorkshop[]>(
-      `https://workshops-server.onrender.com/workshops`,
+      `${this.apiUrl}/workshops`,
     {
         params: {
           _page: page
@@ -24,7 +28,7 @@ export class Workshops {
 
   getWorkshopById(workshopId: number) {
     return this.http.get<IWorkshop>(
-        `https://workshops-server.onrender.com/workshops/${workshopId}`
+        `${this.apiUrl}/workshops/${workshopId}`
     );
   }
 }
