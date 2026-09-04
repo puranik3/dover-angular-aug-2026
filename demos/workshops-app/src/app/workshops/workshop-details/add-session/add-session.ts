27783a1dc4267ea18331ba8aa39f55eabfe485d8
Prashanth Puranik <puranik@digdeeper.in>
2026-09-04 09:47:14 +0530

    Step 33

--- demos/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
+++ demos/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Sessions } from '../../sessions';
+import ISession from '../../models/ISession';
 
 @Component({
   selector: 'app-add-session',
@@ -12,5 +16,34 @@ import { JsonPipe } from '@angular/common';
   styleUrl: './add-session.scss',
 })
 export class AddSession {
+  constructor(
+      private activatedRoute: ActivatedRoute,
+      private sessionsService: Sessions,
+      private router: Router
+  ) {}
   
+  addSession(addSessionForm: NgForm) {
+        const id = +(this.activatedRoute.snapshot.parent?.paramMap.get(
+            'id'
+        ) as string);
+
+        const newSession = {
+            ...addSessionForm.value,
+            workshopId: id,
+            upvoteCount: 0,
+            sequenceId: +addSessionForm.value.sequenceId,
+            duration: +addSessionForm.value.duration,
+        } as Omit<ISession, 'id'>;
+
+        console.log(newSession);
+        
+        this.sessionsService.addSession(newSession).subscribe({
+            next: (addedSession) => {
+                alert(`Added session with id = ${addedSession.id}`);
+                
+                // You can also use navigateByUrl()
+                this.router.navigate(['/workshops', id]);
+            },
+        });
+    }
 }
